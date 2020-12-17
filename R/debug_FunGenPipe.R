36c93b253101e712994a14ccaaaf4ed2bd99c038
Peter Juvan <peter.juvan@gmail.com>
2020-12-17 12:47:55 +0100

    transferred to Ubuntu

--- R/debug_FunGenPipe.R
+++ R/debug_FunGenPipe.R
@@ -20,6 +20,42 @@
 #     message(paste("DECHIPER::MeldDNA start:", timeDECHIPER[["start"]], "finish: ", timeDECHIPER[["finish"]]))
 #     return(tbSeqTm)
 # }
+#### OLD w/o width
+# dechiperMeltDNAtime <- function(myGRanges, myBSgenome, Trange=seq(50,100,1), ions=0.2,
+#     dropMcols=c("seq","Trange","ThetaDerivative","maxThetaDerivative","TmList")) {
+#     require(myBSgenome)
+#     assertthat::assert_that(all(GenomeInfoDb::genome(myGRanges) %in% GenomeInfoDb::genome(myBSgenome))) # test genome names
+#     assertthat::assert_that(all(names(GenomeInfoDb::genome(myGRanges)) %in% GenomeInfoDb::seqnames(myBSgenome))) # test seqnames
+#     timeDECHIPER = list()
+#     timeDECHIPER[["start"]] <- Sys.time()
+#     dss <- Biostrings::getSeq(myBSgenome, myGRanges)
+#     wdss <- which(width(dss) > 2)
+#     # dim(thDer) == Trange x myGRanges[wdss]
+#     thDer.w <- DECIPHER::MeltDNA(dss[wdss], type="derivative", temps=Trange, ions=ions)
+#     thDerList.w <- lapply(seq_len(ncol(thDer.w)), function(i) thDer.w[,i])
+#     tbSeqTm.w <- dss[wdss] %>% 
+#         as.character() %>%
+#         tibble::tibble(name = names(.), seq = ., Trange = list(Trange)) %>% 
+#         dplyr::mutate(
+#             ThetaDerivative = tibble::enframe(thDerList.w)$value,
+#             maxThetaDerivative = purrr::map_dbl(ThetaDerivative, max),
+#             TmList = purrr::pmap(list(Trange, ThetaDerivative, maxThetaDerivative), 
+#                 ~ tryCatch(..1[which(..2 == ..3)], error=function(e) NA)),
+#             Tm = unlist(purrr::map_if(TmList, ~ length(.) != 1, ~ NA)),
+#             tmp_idx = wdss
+#         ) %>% 
+#         dplyr::relocate(Tm)
+#     timeDECHIPER[["finish"]] <- Sys.time()
+#     message(paste("DECHIPER::MeldDNA start:", timeDECHIPER[["start"]], "finish: ", timeDECHIPER[["finish"]]))
+#     S4Vectors::mcols(myGRanges) <- S4Vectors::mcols(myGRanges) %>%
+#         tibble::as_tibble() %>% 
+#         dplyr::mutate(tmp_idx = seq(n())) %>% 
+#         dplyr::left_join(tbSeqTm.w, by="tmp_idx") %>% 
+#         dplyr::select(-tmp_idx, -name) %>% 
+#         dplyr::select(-tidyselect::any_of(dropMcols))
+#     return(myGRanges)
+# }
+
 
 #### plotDistrTargets2
 if (FALSE) {
